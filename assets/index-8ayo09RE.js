0e2048727e1016532c743ef67503b19acb4bedc4
fei <tpf1231000@gmail.com>
2025-08-12 16:48:53 +0800

    Updates

---
+++ assets/index-8ayo09RE.js
@@ -0,0 +1,2 @@
+import{j as e,L as t}from"./index-Dg3-Yd2s.js";const s=function(){return e.jsx(t,{to:"/game",children:e.jsx("div",{className:`w-screen min-h-screen mx-auto bg-[image:var(--bg-home)] bg-cover bg-center aspect-auto 
+      flex flex-col justify-center items-center`,children:e.jsx("p",{className:"text-4xl font-bold text-white",children:" Click to Start"})})})};export{s as component};
--- assets/index-IRb09_2E.js
+++
@@ -1,2 +0,0 @@
-import{j as e,L as t}from"./index-DNH8p8gE.js";const s=function(){return e.jsx(t,{to:"/game",children:e.jsx("div",{className:`w-screen min-h-screen mx-auto bg-[image:var(--bg-home)] bg-cover bg-center aspect-auto 
-      flex flex-col justify-center items-center`,children:e.jsx("p",{className:"text-4xl font-bold text-white",children:" Click to Start"})})})};export{s as component};
